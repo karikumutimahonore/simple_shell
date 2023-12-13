bdd2442a33396b79d8da2a48b21b228d4eb8b325
karikumutimahonore <karikumutimahonore778@gmail.com>
2023-12-13 23:40:37 +0200

    update

--- monday8.c
+++ monday8.c
@@ -8,29 +8,29 @@
  */
 int handle_builtin(shell_t *hons)
 {
-	if (!_strcmp(hons->sub_command[0], "env") ||
-		!_strcmp(hons->sub_command[0], "printenv"))
+	if (!_strcmp(hons->other_cmd[0], "env") ||
+		!_strcmp(hons->other_cmd[0], "printenv"))
 	{
 		_printenv();
 		return (0);
 	}
-	else if (!_strcmp(hons->sub_command[0], "exit"))
+	else if (!_strcmp(hons->other_cmd[0], "exit"))
 	{
 		return (handle_exit(hons, multi_free));
 	}
 
 	/* let's handle the builtin 'cd' command */
-	else if (!_strcmp(hons->sub_command[0], "cd"))
+	else if (!_strcmp(hons->other_cmd[0], "cd"))
 		return (handle_cd(hons));
 
-	else if (!_strcmp(hons->sub_command[0], "setenv"))
+	else if (!_strcmp(hons->other_cmd[0], "setenv"))
 	{
-		if (hons->sub_command[1] && hons->sub_command[2])
-			return (setenv(hons->sub_command[1], hons->sub_command[2], 1));
+		if (hons->other_cmd[1] && hons->other_cmd[2])
+			return (setenv(hons->other_cmd[1], hons->other_cmd[2], 1));
 		return (1); /* invalid number of parameters received */
 	}
-	else if (!_strcmp(hons->sub_command[0], "unsetenv"))
-		return (_unsetenv(hons->sub_command[1]));
+	else if (!_strcmp(hons->other_cmd[0], "unsetenv"))
+		return (_unsetenv(hons->other_cmd[1]));
 
 	return (NOT_BUILTIN); /* not a builtin command */
 }
